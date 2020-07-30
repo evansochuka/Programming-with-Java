0faf3c553de79c422b7ebcc6a2488ac85e8d9750
evansochuka <evansochuka.254@gmail.com>
2020-07-30 07:41:00 +0300

    Add files via upload

---
+++ FirstApp/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+/*
+This is first line comment
+This is second line comment
+ */
+    public static void main(String[] args) {
+        System.out.println("First line from App");//This ias a line comment
+        System.out.println("Second line from App");
+        System.out.println("First line from App");
+    }
+}
